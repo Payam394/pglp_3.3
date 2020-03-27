3a758d305cd1013ed07addee4346eb9cd5ac0b89
payam394 <payam.ak394@gmail.com>
2020-03-27 22:42:35 +0100

    ajouter class Robot

---
+++ src/main/java/Robot.java
@@ -0,0 +1,17 @@
+
+public class Robot {
+
+	 private private
+	 private Position position ;
+	 private Direction direction ;
+	 
+	 
+	 
+	 public void avance(){ 
+	    }
+	 
+	 public void tourne () {
+		 
+	 }
+	   
+}
